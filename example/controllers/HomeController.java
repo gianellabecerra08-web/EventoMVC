f28fb9d90bd7223f2111b5fa03e8d1d3db02814f
GIANELLA B H <gianellabecerra08@gmail.com>
2026-04-18 20:19:57 -0500

    Tabs nuevos agregados al sistema

--- example/controllers/HomeController.java
+++ example/controllers/HomeController.java
@@ -9,18 +9,18 @@ import views.NewEventView;
 /**
  * Main controller. It will be responsible for program's main screen behavior.
  */
-public class HomeController extends Controller 
+public class HomeController extends Controller
 {
 	//-----------------------------------------------------------------------
-	//		Attributes
+	//    Attributes
 	//-----------------------------------------------------------------------
 	private HomeView homeView;
 	private EventListController eventListController = new EventListController();
 	private NewEventController newEventController = new NewEventController(eventListController);
-	
-	
+
+
 	//-----------------------------------------------------------------------
-	//		Methods
+	//    Methods
 	//-----------------------------------------------------------------------
 	@Override
 	public void run()
@@ -28,26 +28,32 @@ public class HomeController extends Controller
 		// Initializes others controllers
 		eventListController.run();
 		newEventController.run();
-		
+
 		// Initializes HomeView
 		homeView = new HomeView(this, mainFrame);
 		addView("HomeView", homeView);
-		
+
 		// Displays the program window
 		mainFrame.setVisible(true);
 	}
-	
-	
+
+
 	//-----------------------------------------------------------------------
-	//		Getters
+	//    Getters
 	//-----------------------------------------------------------------------
 	public EventListView getEventListView()
 	{
 		return eventListController.getView();
 	}
-	
+
 	public NewEventView getNewEventView()
 	{
 		return newEventController.getView();
+	}
+
+	// NUEVO: necesario para RemoveEventView
+	public EventListController getEventListController()
+	{
+		return eventListController;
 	}
 }
--- example/views/HomeView.java
+++ example/views/HomeView.java
@@ -13,73 +13,59 @@ import core.View;
 
 
 /**
- * View associated with {@link HomeController}. It will be responsible for program's 
- * main screen view.
+ * View associated with HomeController. Main screen with all tabs.
  */
 @SuppressWarnings("serial")
 public class HomeView extends JPanel implements View
 {
-	//-----------------------------------------------------------------------
-	//		Attributes
-	//-----------------------------------------------------------------------
 	@SuppressWarnings("unused")
 	private HomeController homeController;
 	private JFrame mainFrame;
-	private final static int MAIN_FRAME_WIDTH = 500;
-	private final static int MAIN_FRAME_HEIGHT = 350;
+	private final static int MAIN_FRAME_WIDTH  = 600;
+	private final static int MAIN_FRAME_HEIGHT = 400;
 	private final static int MAIN_FRAME_X = 100;
 	private final static int MAIN_FRAME_Y = 100;
-	
-	
-	//-----------------------------------------------------------------------
-	//		Constructor
-	//-----------------------------------------------------------------------
-	/**
-	 * Initializes the view responsible for program's main screen.
-	 * 
-	 * @param homeController Controller responsible for this view
-	 * @param mainFrame Main frame / window of the application
-	 */
+
 	public HomeView(HomeController homeController, JFrame mainFrame)
 	{
 		this.homeController = homeController;
 		this.mainFrame = mainFrame;
-		
+
 		make_mainFrame();
 		make_tabs();
 	}
-	
-	
-	//-----------------------------------------------------------------------
-	//		Methods
-	//-----------------------------------------------------------------------
+
 	@Override
-	public void update(Model model, Object data) 
-	{}
-	
-	/**
-	 * Configures the main frame of the application.
-	 */
+	public void update(Model model, Object data) {}
+
 	private void make_mainFrame()
 	{
 		mainFrame.setOpacity(1.0f);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setBounds(MAIN_FRAME_X, MAIN_FRAME_Y, MAIN_FRAME_WIDTH, MAIN_FRAME_HEIGHT);
 		mainFrame.setMinimumSize(new Dimension(MAIN_FRAME_WIDTH, MAIN_FRAME_HEIGHT));
-		
-		
+
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLayout(new BorderLayout(0, 0));
 	}
-	
-	/**
-	 * Creates tab navigation.
-	 */
+
 	private void make_tabs()
 	{
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+
+		// Tab 1: Crear evento (ya existía)
 		tabbedPane.addTab("New event", homeController.getNewEventView());
+
+		// Tab 2: Listar eventos (ya existía)
 		tabbedPane.addTab("Events", homeController.getEventListView());
+
+		// Tab 3: Eliminar evento (NUEVO)
+		tabbedPane.addTab("Remove Event",
+				new RemoveEventView(homeController.getEventListController()));
+
+		// Tab 4: Registrar invitado (NUEVO)
+		tabbedPane.addTab("Registrar invitado", new RegistrarInvitadoView());
+
 		add(tabbedPane, BorderLayout.CENTER);
 	}
 }
